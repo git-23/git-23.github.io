5ea5c28e3e241986b3bf3c3e8b65382556be0f0f
git-23 <3378840772@qq.com>
2023-07-27 12:21:50 +0800

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,62 @@
-let docsList = ['markdown.md',
-                'dom.md',
-                'commonJS.md',
-                'html&css.md',
-                'html5.md',
-                'javascript.md'];
+let bookList = [
+    ['JavaScript 高级程序设计', true],
+    ['ES6标准入门', false],
+], docsList = [
+    'markdown.md',
+    'dom.md',
+    'html&css.md',
+    'html5.md',
+    'javascript.md',
+];
 
-function renderButton(docsList) {
-    for (let item in docsList) {
-        let Item = document.createElement('a');
-        Item.innerText = docsList[item];
-        let ListItemNode = document.createElement('li');
-        ListItemNode.appendChild(Item);
-        document.getElementById('menu').insertAdjacentElement('beforeend', ListItemNode);
-        ListItemNode.addEventListener('click', function () {renew(docsList[item])});
+function addEventListenerInButton() {
+    let todo = document.getElementById('todo');
+    let list = document.getElementById('list');
+    let page = document.getElementById('page');
+
+    todo.addEventListener('click', clickTodo);
+    list.addEventListener('click', clickList);
+    page.addEventListener('click', () => void window.open('https://git-23.github.io/running_page/', '_self'));
+}
+
+function clickTodo() {
+    let todoSection = document.createElement('ol');
+    for (let item of bookList) {
+        let bookItem = document.createElement('li');
+        bookItem.innerHTML = item[0];
+        bookItem.style.textDecoration = item[1] ? 'line-through' : 'none';
+        todoSection.append(bookItem);
+    }
+    let container = document.getElementById('container');
+    container.innerHTML = '';
+    container.append(todoSection);
+}
+
+function clickList() {
+    let container = document.getElementById('container');
+    container.innerHTML = '';
+    for (let item of docsList) {
+        let docsItem = document.createElement('button');
+        docsItem.innerHTML = item;
+        container.append(docsItem);
+        docsItem.addEventListener('click', () => void displayNote(item))
     }
 }
 
-function renew(docsName) {
-    console.log('ddd' + docsName)
+function displayNote(item) {
+    let url = './docs/' + item;
     let xhr = new XMLHttpRequest();
-
-    docsName = './docs/' + docsName;
-    xhr.open('GET', docsName, true);
+    xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var markdownText = xhr.responseText;
-        
-        // 在这里处理markdownText
-        document.getElementById('noteDisplay').innerHTML = marked.parse(markdownText);
-        hljs.highlightAll()
-    }
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const markdownDOM = marked.parse(xhr.responseText);
+            let container = document.getElementById('container');
+            container.innerHTML = markdownDOM;
+            hljs.highlightAll()
+        }
     };
     xhr.send();
 }
 
-renderButton(docsList);
+addEventListenerInButton();
+
